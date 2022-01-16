3eeec11f0ea383a20d063ec99277235b7247d9e6
Shane Blume <shanesblume@gmail.com>
2022-01-16 15:09:13 -0500

    Running pod install and updating ios build settings.

---
+++ lib/core/viewmodels/game_crud_model.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:pick/core/models/game.dart';
+import 'package:pick/core/models/sport_model.dart';
+
+class GameCrudModel {
+  List<Game> games = [
+    Game(
+      id: 0,
+      name: 'Pick\'em',
+      iconData: Icons.sports_football,
+    ),
+    Game(
+      id: 1,
+      name: 'Squares',
+      iconData: Icons.sports_basketball,
+    ),
+  ];
+
+  List<Game> fetchGames() {
+    return games;
+  }
+
+  Game getGameById(int id) {
+    return games.firstWhere((game) => game.id == id);
+  }
+
+  Game getGameByName(String name) {
+    return games
+        .firstWhere((game) => game.name.toLowerCase() == name.toLowerCase());
+  }
+}
