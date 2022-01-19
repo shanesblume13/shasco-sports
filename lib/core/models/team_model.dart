f8da1b4afa2a62f92e20fd42973eb3bf06fc39aa
Shane Blume <shanesblume@gmail.com>
2022-01-18 20:07:46 -0500

    Adding initial team model properties.

---
+++ lib/core/models/team_model.dart
@@ -0,0 +1,15 @@
+class Team {
+  int id;
+  String league;
+  String location;
+  String nickname;
+  String imagePath;
+
+  Team({
+    required this.id,
+    required this.league,
+    required this.location,
+    required this.nickname,
+    required this.imagePath,
+  });
+}
