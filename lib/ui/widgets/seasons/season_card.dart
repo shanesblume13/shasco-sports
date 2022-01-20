1acb3a9c184e9712e4e8bcb0f95e789fba6b3aa4
Shane Blume <shanesblume@gmail.com>
2022-01-19 20:46:55 -0500

    Including missed season widgets.

---
+++ lib/ui/widgets/seasons/season_card.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_layout_grid/flutter_layout_grid.dart';
+import 'package:pick/core/models/season_model.dart';
+import 'package:pick/ui/widgets/seasons/season_card_image_clip.dart';
+import 'package:pick/ui/widgets/seasons/season_card_name_container.dart';
+import 'package:pick/ui/widgets/seasons/season_card_leg_count_container.dart';
+
+class SeasonCard extends StatelessWidget {
+  const SeasonCard({
+    Key? key,
+    required this.season,
+  }) : super(key: key);
+
+  final Season season;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        null;
+        // Navigator.push(
+        //   context,
+        //   MaterialPageRoute(
+        //     builder: (_) => LeaguesView(
+        //       sport: sport.name,
+        //     ),
+        //   ),
+        // );
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Card(
+          elevation: 6,
+          child: LayoutGrid(
+            areas: '''
+                image name legCount
+              ''',
+            rowSizes: const [
+              auto,
+            ],
+            columnSizes: [
+              1.fr,
+              3.fr,
+              2.fr,
+            ],
+            children: [
+              gridArea('image').containing(
+                const SeasonCardImageClip(),
+              ),
+              gridArea('name').containing(
+                SeasonCardNameConatiner(name: season.name),
+              ),
+              gridArea('legCount').containing(
+                // TODO: Figure out how to use seasonId
+                SeasonCardLegCountContainer(seasonId: season.id),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
