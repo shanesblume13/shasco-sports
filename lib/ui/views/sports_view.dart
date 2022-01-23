6732acfffed37713cace558367c854418112eac4
Shane Blume <shanesblume@gmail.com>
2022-01-23 13:50:55 -0500

    Adding riverpod_hooks. Converting Sport to use riverpod.

--- lib/ui/views/sports_view.dart
+++ lib/ui/views/sports_view.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pick/core/models/sport_model.dart';
-import 'package:pick/core/viewmodels/sport_view_model.dart';
+import 'package:pick/core/viewmodels/sport_provider.dart';
 import 'package:pick/ui/widgets/sports/sport_cards_listview.dart';
-import 'package:provider/provider.dart';
 
-class SportsView extends StatelessWidget {
+class SportsView extends HookConsumerWidget {
   const SportsView({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final sportProvider = Provider.of<SportViewModel>(context);
-    final List<Sport> sports = sportProvider.fetchSports();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final List<Sport> sports = ref.watch(sportProvider);
 
     return Scaffold(
-        appBar: AppBar(
-          title: const Center(
-            child: Text('Sports'),
-          ),
+      appBar: AppBar(
+        title: const Center(
+          child: Text('Sports'),
         ),
-        body: SportCardsListview(sports: sports));
+      ),
+      body: SportCardsListview(sports: sports),
+    ); //
   }
 }
