dda16c0f8ca051230ddbdd7ebbce5059d94bc4b1
Shane Blume <shanesblume@gmail.com>
2022-01-24 11:42:57 -0500

    Removing iconColor from Sport model.

--- lib/core/models/sport_model.dart
+++ lib/core/models/sport_model.dart
@@ -4,13 +4,11 @@ class Sport {
   int id;
   String name;
   IconData iconData;
-  Color iconColor;
 
   Sport({
     required this.id,
     required this.name,
     required this.iconData,
-    required this.iconColor,
   });
 
   static List<Sport> sports = [
@@ -18,13 +16,11 @@ class Sport {
       id: 0,
       name: 'Football',
       iconData: Icons.sports_football,
-      iconColor: Colors.brown,
     ),
     Sport(
       id: 1,
       name: 'Basketball',
       iconData: Icons.sports_basketball,
-      iconColor: Colors.orange,
     ),
   ];
 }
