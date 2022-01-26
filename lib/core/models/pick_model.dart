5f05edfc33c34248d3f314e91f550f0093e66b4c
Shane Blume <shanesblume@gmail.com>
2022-01-26 15:05:40 -0500

    Adding leg reference to pick model.

--- lib/core/models/pick_model.dart
+++ lib/core/models/pick_model.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Pick {
   String id;
   String uid;
+  DocumentReference legReference;
   DocumentReference matchupReference;
   DocumentReference? teamReference;
   int points;
@@ -10,6 +11,7 @@ class Pick {
   Pick({
     required this.id,
     required this.uid,
+    required this.legReference,
     required this.matchupReference,
     this.teamReference,
     required this.points,
@@ -17,12 +19,14 @@ class Pick {
 
   Pick.fromQueryDocumentSnapshot(QueryDocumentSnapshot snapshot, this.id)
       : uid = snapshot['uid'] as String,
+        legReference = snapshot['legReference'] as DocumentReference,
         matchupReference = snapshot['matchupReference'] as DocumentReference,
         teamReference = snapshot['teamReference'] as DocumentReference,
         points = snapshot['points'] as int;
 
   Pick.fromDocumentSnapshot(DocumentSnapshot snapshot, this.id)
       : uid = snapshot['uid'] as String,
+        legReference = snapshot['legReference'] as DocumentReference,
         matchupReference = snapshot['matchupReference'] as DocumentReference,
         teamReference = snapshot['teamReference'] as DocumentReference,
         points = snapshot['points'] as int;
@@ -30,6 +34,7 @@ class Pick {
   toJson() {
     return {
       'uid': uid,
+      'legReference': legReference,
       'matchupReference': matchupReference,
       'teamReference': teamReference,
       'points': points,
